780a63fbe0f0703ac453c6559d8163595bd2c1c4
Vonney <davonnejv@gmail.com>
2023-06-21 04:13:49 -0500

    creating onClick to name color when selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
+import { useState } from 'react';
+
+
+
 //Color Component
-const Color = ({color}) => {
-  return <div className={color}></div>
-}
+const Color = ({color, setSelectedColor}) => {
+  return  (
+  
+  <div className={color}
+  onClick={() => setSelectedColor(color)}>
+  </div>
+
+  )
+}  //last curly of color component 
+
+
 
 //App Component 
 const App = () => {
+const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
 
       <div id="colors-list">
-        <Color color="violet"/>
-        <Color color="black"/>
-        <Color color="blue"/>
+        <Color color="violet" setSelectedColor={setSelectedColor}/>
+        <Color color="black" setSelectedColor={setSelectedColor}/>
+        <Color color="blue" setSelectedColor={setSelectedColor}/>
       </div>
     </div>  //last div of container
   );
 };
 
 export default App;
+
+
+
